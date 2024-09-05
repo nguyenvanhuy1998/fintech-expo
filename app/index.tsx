37c83b81a1a514eadbb028d781301cffbba78c2a
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2024-09-05 22:38:58 +0700

    first commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const Layout = () => {
+const Page = () => {
     return (
         <View>
-            <Text>Layout</Text>
+            <Text>Page</Text>
         </View>
     );
 };
 
-export default Layout;
+export default Page;
