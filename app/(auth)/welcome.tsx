87b31b5b29fe6f8d70f2ae2b43d7fb33c43649ed
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2024-09-06 18:03:04 +0700

    add native wind css

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const Welcome = () => {
     return (
-        <View>
-            <Text>Welcome</Text>
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="font-JakartaSemiBold">Welcome</Text>
         </View>
     );
 };
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,9 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Redirect } from "expo-router";
 
 const Page = () => {
-    return (
-        <View>
-            <Text>Page</Text>
-        </View>
-    );
+    return <Redirect href={"/(auth)/welcome"} />;
 };
 
 export default Page;
