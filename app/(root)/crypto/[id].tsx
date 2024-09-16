9916559d0b8089f88b381c040d0b39d77db85921
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2024-09-16 15:28:13 +0700

    Victory Chart with Reanimated

--- app/(root)/crypto/[id].tsx
+++ app/(root)/crypto/[id].tsx
@@ -6,20 +6,67 @@ import {
     TouchableOpacity,
     ScrollView,
     StyleSheet,
+    TextInput,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Stack, useLocalSearchParams } from "expo-router";
 import { useHeaderHeight } from "@react-navigation/elements";
 import { defaultStyles } from "../../../constants/styles";
 import { useQuery } from "@tanstack/react-query";
 import { Ionicons } from "@expo/vector-icons";
 import { Colors } from "@/constants";
+import { CartesianChart, Line, useChartPressState } from "victory-native";
+import { Circle, useFont } from "@shopify/react-native-skia";
+import { format } from "date-fns";
+import * as Haptics from "expo-haptics";
+import Animated, {
+    SharedValue,
+    useAnimatedProps,
+} from "react-native-reanimated";
 
 const categories = ["Overview", "News", "Orders", "Transactions"];
+// Animated.addWhitelistedNativeProps({
+//     text: true,
+// });
+const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
+function ToolTip({ x, y }: { x: SharedValue<number>; y: SharedValue<number> }) {
+    return <Circle cx={x} cy={y} r={8} color={Colors.primary} />;
+}
+
 const CryptoDetail = () => {
     const { id } = useLocalSearchParams();
     const headerHeight = useHeaderHeight();
     const [activeIndex, setActiveIndex] = useState(0);
+    const font = useFont(
+        require("@/assets/fonts/PlusJakartaSans-Regular.ttf"),
+        12
+    );
+    const { state, isActive } = useChartPressState({
+        x: 0,
+        y: {
+            price: 0,
+        },
+    });
+    const animatedPriceText = useAnimatedProps(() => {
+        return {
+            text: `${state.y.price.value.value.toFixed(2)} €`,
+            defaultValue: "",
+        };
+    });
+    const animatedDateText = useAnimatedProps(() => {
+        const date = new Date(state.x.value.value);
+        return {
+            text: date.toLocaleDateString(),
+            defaultValue: "",
+        };
+    });
+
+    useEffect(() => {
+        console.log({ isActive });
+        if (isActive) {
+            Haptics.selectionAsync();
+        }
+    }, [isActive]);
 
     const { data } = useQuery({
         queryKey: ["info", id],
@@ -29,6 +76,11 @@ const CryptoDetail = () => {
             );
             return info[+id];
         },
+    });
+    const { data: tickers } = useQuery({
+        queryKey: ["tickers"],
+        queryFn: async (): Promise<any[]> =>
+            fetch(`/api/tickers`).then((res) => res.json()),
     });
     return (
         <>
@@ -149,9 +201,76 @@ const CryptoDetail = () => {
                 )}
                 renderItem={({ item }) => (
                     <>
-                        {/* TODO: CHART */}
-                        <View className="h-[500px] bg-green-500">
-
+                        <View style={defaultStyles.block} className="h-[500px]">
+                            {tickers && (
+                                <>
+                                    {!isActive && (
+                                        <View>
+                                            <Text className="text-3xl font-JakartaBold text-dark">
+                                                {tickers[
+                                                    tickers.length - 1
+                                                ].price.toFixed(2)}
+                                                €
+                                            </Text>
+                                            <Text className="text-lg font-Jakarta text-gray">
+                                                Today
+                                            </Text>
+                                        </View>
+                                    )}
+                                    {isActive && (
+                                        <View>
+                                            <AnimatedTextInput
+                                                editable={false}
+                                                underlineColorAndroid={
+                                                    "transparent"
+                                                }
+                                                animatedProps={
+                                                    animatedPriceText
+                                                }
+                                                className="text-3xl font-JakartaBold text-dark"
+                                            />
+                                            <AnimatedTextInput
+                                                className="text-lg font-Jakarta text-gray"
+                                                animatedProps={animatedDateText}
+                                            />
+                                        </View>
+                                    )}
+                                    <CartesianChart
+                                        chartPressState={state}
+                                        axisOptions={{
+                                            font,
+                                            tickCount: 5,
+                                            labelOffset: { x: -2, y: 0 },
+                                            labelColor: Colors.gray,
+                                            formatYLabel: (v) => `${v} €`,
+                                            formatXLabel: (ms) =>
+                                                format(new Date(ms), "MM/yy"),
+                                        }}
+                                        data={tickers!}
+                                        xKey="timestamp"
+                                        yKeys={["price"]}
+                                    >
+                                        {({ points }) => (
+                                            <>
+                                                <Line
+                                                    points={points.price}
+                                                    color={Colors.primary}
+                                                    strokeWidth={3}
+                                                />
+                                                {isActive && (
+                                                    <ToolTip
+                                                        x={state.x.position}
+                                                        y={
+                                                            state.y.price
+                                                                .position
+                                                        }
+                                                    />
+                                                )}
+                                            </>
+                                        )}
+                                    </CartesianChart>
+                                </>
+                            )}
                         </View>
 
                         <View style={defaultStyles.block} className="mt-5">
